7fb360cb6a756c9b144d152f5cbc7d9029f6d1ca
EhvDS <127723158+EhvDS@users.noreply.github.com>
2024-10-16 17:43:43 +0200

    Update PROOFS_OF_CONCEPT.py

--- PROOFS_OF_CONCEPT.py
+++ PROOFS_OF_CONCEPT.py
@@ -4,7 +4,7 @@ import streamlit as st
 def main():
     st.set_page_config(
         page_title='Interactive Tools for Sustainable Cities' ,
-        page_icon='',   
+        page_icon="🌿",
         layout="wide",
         initial_sidebar_state="expanded")
 
