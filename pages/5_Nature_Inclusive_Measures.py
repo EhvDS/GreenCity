880e61d74f89773b3c43030ec8a85c38f989ea86
Youss <youss.eljaddaoui@gmail.com>
2024-10-25 23:54:07 +0200

    Update 5_Nature_Inclusive_Measures.py

--- pages/5_Nature_Inclusive_Measures.py
+++ pages/5_Nature_Inclusive_Measures.py
@@ -66,17 +66,17 @@ for i in range(0, len(filtered_items), 4):  # Loop through items with a step of 
 # Show details for the selected item
 if selected_item_name:
     selected_item = next(item for item in items if item["name"] == selected_item_name)
-    st.subheader(selected_item["name"])
+    st.header(selected_item["name"])
 
-    # Sections
-    st.subheader("Description")
-    for section in selected_item["sections"]:
-        st.write(f"**{section['header']}**")
-        st.write(section["text"])
+    # # Sections
+    # st.subheader("Description")
+    # for section in selected_item["sections"]:
+    #     st.write(f"**{section['header']}**")
+    #     st.write(section["text"])
 
-    # Guidelines
-    st.markdown("<br/><br/>", unsafe_allow_html=True)
-    st.subheader("Guidelines")
-    for guideline in selected_item["guidelines"]["options"]:
-        st.write(f"**{guideline['title']}**")
-        st.write(guideline["text"])
+    # # Guidelines
+    # st.markdown("<br/><br/>", unsafe_allow_html=True)
+    # st.subheader("Guidelines")
+    # for guideline in selected_item["guidelines"]["options"]:
+    #     st.write(f"**{guideline['title']}**")
+    #     st.write(guideline["text"])
