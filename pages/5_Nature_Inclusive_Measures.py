244a1b8f73456310743435e3d590954a7cd37896
Youss <youss.eljaddaoui@gmail.com>
2024-10-25 23:44:19 +0200

    Update 5_Nature_Inclusive_Measures.py

--- pages/5_Nature_Inclusive_Measures.py
+++ pages/5_Nature_Inclusive_Measures.py
@@ -44,12 +44,6 @@ st.markdown("""
         display: flex;
         flex-direction: column;
     }
-    /* Remove unwanted borders or lines from components */
-    .block-container {
-        border: none !important;
-        box-shadow: none !important;
-        padding: 0 !important;
-    }
     </style>
 """, unsafe_allow_html=True)
 
@@ -72,7 +66,7 @@ if selected_item_name:
 
     # Sections
     st.subheader("Description")
-    st.markdown("<hr style='border:1px solid gray;'>", unsafe_allow_html=True)  # Keep intended hr
+    st.markdown("<hr style='border:1px solid gray;'>", unsafe_allow_html=True)
     for section in selected_item["sections"]:
         st.write(f"**{section['header']}**")
         st.write(section["text"])
@@ -80,7 +74,7 @@ if selected_item_name:
     # Guidelines
     st.markdown("<br/><br/>", unsafe_allow_html=True)
     st.subheader("Guidelines")
-    st.markdown("<hr style='border:1px solid gray;'>", unsafe_allow_html=True)  # Keep intended hr
+    st.markdown("<hr style='border:1px solid gray;'>", unsafe_allow_html=True)
     for guideline in selected_item["guidelines"]["options"]:
         st.write(f"**{guideline['title']}**")
         st.write(guideline["text"])
