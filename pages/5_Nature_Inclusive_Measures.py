649ee272b16678a0eec7b986792cc7b80267b7e9
Youss <youss.eljaddaoui@gmail.com>
2024-10-25 23:54:58 +0200

    Update 5_Nature_Inclusive_Measures.py

--- pages/5_Nature_Inclusive_Measures.py
+++ pages/5_Nature_Inclusive_Measures.py
@@ -70,13 +70,13 @@ if selected_item_name:
 
     # Sections
     st.subheader("Description")
-    for section in selected_item["sections"]:
-        st.write(f"**{section['header']}**")
-        st.write(section["text"])
+    # for section in selected_item["sections"]:
+    #     st.write(f"**{section['header']}**")
+    #     st.write(section["text"])
 
-    # Guidelines
-    st.markdown("<br/><br/>", unsafe_allow_html=True)
-    st.subheader("Guidelines")
-    for guideline in selected_item["guidelines"]["options"]:
-        st.write(f"**{guideline['title']}**")
-        st.write(guideline["text"])
+    # # Guidelines
+    # st.markdown("<br/><br/>", unsafe_allow_html=True)
+    # st.subheader("Guidelines")
+    # for guideline in selected_item["guidelines"]["options"]:
+    #     st.write(f"**{guideline['title']}**")
+    #     st.write(guideline["text"])
