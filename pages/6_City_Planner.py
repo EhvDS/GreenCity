fff6c52979e3a3a8006b53f541bd961d3bd2cec5
Antonio Takev <tonitakev@gmail.com>
2024-10-28 15:11:28 +0100

    feat: Add City planner web app

---
+++ pages/6_City_Planner.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("City Planner web app 🏙️")
+st.markdown(
+    "City Planner is web app that contributes to the stakeholder engagement of city residents in the planning of urban sustainable projects."
+)
+st.markdown("Link to the project: [City Planner](https://city-planner.netlify.app/)")
